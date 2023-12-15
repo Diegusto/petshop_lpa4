a0c287b5e4f1144892730191ca914e8e3913a3f4
Diegusto <deigusto35@gmail.com>
2023-12-14 21:35:25 -0300

    fix:login

--- projeto petshop/Form1.cs
+++ projeto petshop/Form1.cs
@@ -1,4 +1,5 @@
 using System.Data.SqlClient;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.StartPanel;
 
 namespace projeto_petshop
 {
@@ -8,6 +9,8 @@ namespace projeto_petshop
         {
             InitializeComponent();
         }
+        string username;
+        string password;
 
 
         private void Exitbtn_Click(object sender, EventArgs e)
@@ -19,15 +22,46 @@ namespace projeto_petshop
         {
             string usuario = textBox1.Text;
             string senha = textBox2.Text;
-            if (usuario == "admin" && senha == "admin")
+
+            Connection connection = new Connection();
+            SqlCommand sqlCommand = new SqlCommand();
+
+            connection.OpenConnection();
+
+            sqlCommand.Connection = connection.ReturnConnection();
+            try
             {
-                this.Hide();
-                Form2 f2 = new Form2();
-                f2.Show();
+                sqlCommand.CommandText = "select nome, senha from clientes where nome = @nome and senha = @senha";
+                sqlCommand.Parameters.AddWithValue("@nome", usuario);
+                sqlCommand.Parameters.AddWithValue("@senha", senha);
+
+                SqlDataReader reader = sqlCommand.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    username = reader[0].ToString();
+                    password = reader[1].ToString();
+                   
+
+                }
+                if (!string.IsNullOrEmpty(username) && !string.IsNullOrEmpty(password))
+                {
+                    this.Hide();
+                    Form2 f2 = new Form2();
+                    f2.Show();
+                }
+                else
+                {
+                    MessageBox.Show("login incorreto");
+                }
             }
-            else
+            catch (Exception ex)
             {
-                MessageBox.Show("login incorreto");
+                throw new Exception("erro" + ex.Message);
+            }
+            finally
+            {
+                connection.CloseConnection();
             }
         }
     }
